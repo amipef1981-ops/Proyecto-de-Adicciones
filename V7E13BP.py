5d012b4fa8687643d8b53b2db8b2d5b313c70dbe
amipef1981-ops <amipef1981@gmail.com>
2025-12-08 11:00:40 -0600

    Add files via upload
    Creamos un código donde podíamos observar las edades de distintas personas

---
+++ V7E13BP.py
@@ -0,0 +1,14 @@
+edades = [22, 14, 33, 11, 74, 35, 27, 9]
+print(edades[6])
+print("_______________________________")
+
+edades[0]=18
+print(edades[0])
+print("_______________________________")
+print(edades[-1])
+tamanio=len(edades)
+print(edades[tamanio -1])
+print("_______________________________")
+
+for e in edades:
+    print(e)
